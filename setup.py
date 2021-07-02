e6b323ee99bdbc4a4ed1893cc26fb59144211998
Jawayria <jawayriahashmi@gmail.com>
2021-07-02 20:04:23 +0500

    feat: Added support for django3.2

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.0",
         "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ]
